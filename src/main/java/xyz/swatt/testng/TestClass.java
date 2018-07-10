f7366ce0f6967483db91d272c1fd41a9c7ef7d3f
BrandonDudek <19555354+BrandonDudek@users.noreply.github.com>
2018-07-10 09:40:57 -0400

    Added Test and Factory DataProviders.

--- src/main/java/xyz/swatt/testng/TestClass.java
+++ src/main/java/xyz/swatt/testng/TestClass.java
@@ -2,10 +2,42 @@ package xyz.swatt.testng;
 
 import org.testng.annotations.*;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
 /**
  * The Class doing the Tests.
  */
 public class TestClass {
+
+    @DataProvider
+    public static Iterator< Object[] > factoryDataProvider() {
+
+        System.out.println("Factory\tDataProvider");
+
+        final int COUNT = 13;
+
+        List<Object[]> toRet = new ArrayList( COUNT );
+
+        for( int i = 0; i < COUNT; i++ ) {
+            char c = (char) ( 'a' + new Random().nextInt( 26 ) );
+            toRet.add( new Object[] { String.valueOf(c) } );
+        }
+
+        return toRet.iterator();
+    }
+
+    private final String INSTANCE_NAME;
+
+    @Factory(dataProvider = "factoryDataProvider")
+    public TestClass(String _instanceName) {
+
+        System.out.println("Factory\t" + _instanceName);
+
+        INSTANCE_NAME = _instanceName;
+    }
 
     @BeforeSuite
     public void beforeSuite() {
@@ -27,9 +59,25 @@ public class TestClass {
         System.out.println("Before\tMethod");
     }
 
-    @Test
-    public void testMethod() {
-        System.out.println("Test\tMethod");
+    @DataProvider
+    public Iterator< Object[] > testMethodDataProvider() {
+
+        System.out.println("TestMethod\tDataProvider");
+
+        final int COUNT = 3;
+
+        List<Object[]> toRet = new ArrayList( COUNT );
+
+        for( int i = 0; i < COUNT; i++ ) {
+            toRet.add( new Object[] { String.valueOf(i) } );
+        }
+
+        return toRet.iterator();
+    }
+
+    @Test(dataProvider = "testMethodDataProvider")
+    public void testMethod(String _testName) {
+        System.out.println("Test\tMethod " + INSTANCE_NAME + "-" + _testName);
     }
 
     @AfterMethod
@@ -52,4 +100,8 @@ public class TestClass {
         System.out.println("After\tSuite");
     }
 
+    @Override
+    public String toString() {
+        return INSTANCE_NAME;
+    }
 }
